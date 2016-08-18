6f09ba978302fbaefbab7826b22a439ff77f6c4a
Joe Bernardi <joebernardi@hotmail.com>
2016-08-17 21:31:02 -0500

    merge sort

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,39 +1,45 @@
+require 'pry'
+
 def merge_sort( array )
 
-    # break the array into small lists of 2
-    # sort each small list of 2
-    # merge the sorted lists into sorted lists of 4
-    # continue merging until one list remainces
-    # mergeSort the left half
-    # mergeSort the right half
-    # merge the two halves
-    # return merged array
+	return array if array.count <= 1
 
+    middle = ( array.count / 2 ).floor
 
-end
+    left_arr = merge_sort( array[ 0..middle - 1 ] )
+    right_arr = merge_sort( array[ ( middle )..( array.count - 1 ) ] )
 
-
-def break_up( array )
-
-	# starting at the first index we take the first 2 elements
-	# create and array with them
-	# send them to the merge sort
-	# merge sort returns the sorted array
-	# if % 2 != 0 => left half will be (arr.length / 2 + 1)
-	# right half will be every two elements unless one left
-
-end
-
-
-def merge( array_1, array_2 )
-
+    merge( left_arr, right_arr )
 
 end
 
 
 
-def sort( array )
 
+def merge( left, right )
+
+
+  if left.empty?
+
+  	return right
+
+  elsif right.empty?
+
+  	return left
+
+  elsif left[ 0 ] < right[ 0 ]
+
+    [ left[ 0 ] ] + merge( left[ 1..left.count ], right )
+
+  else
+
+    [ right[ 0 ] ] + merge( left, right[ 1..right.count ] )
+
+  end
 
 
 end
+
+
+arr = merge_sort( [ 4, 7, 1, -34, 10, 99, 81, 14, 2, 7 ] )
+print arr
